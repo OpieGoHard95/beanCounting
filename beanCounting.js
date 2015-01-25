47bfa9b6ebe93bf589de4b1881f312515a80df25
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-01-24 23:31:48 -0500

    Finished the second step. Easier than I thought :)

--- beanCounting.js
+++ beanCounting.js
@@ -1,9 +1,17 @@
-var countBs = function(bstring) {
+var countBs = function(bstring,countChar) {
   var bcounter = 0;
   var scounter = bstring.length;
   for (var cat = 0; cat <= scounter; cat++)
-    if (bstring.charAt(cat) == "B")
+    if (bstring.charAt(cat) == countChar)
       bcounter = bcounter + 1;
   return bcounter;
 }
-console.log(countBs("Ben plays Basketball in Boise"))
+console.log(countBs("Ben plays Basketball in Boise with his friends","e"))
+
+
+
+
+
+
+
+
